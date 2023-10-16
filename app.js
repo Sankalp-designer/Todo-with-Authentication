bc1c5b778e9e7ba838f23268c29d2bf57c83b432
Sankalp-designer <duttsankalp6@gmail.com>
2023-10-16 16:11:22 +0530

    updated cors again

--- app.js
+++ app.js
@@ -17,6 +17,18 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(userRouter);
 app.use(taskRouter);
+app.use(function(req, res, next) {
+    // res.header("Access-Control-Allow-Origin", "*");
+    const allowedOrigins = ['http://localhost:4000', 'https://todo-with-authentication.onrender.com/'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+         res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+    next();
+  });
 app.use(cors({
     origin: true,
     methods: ["POST", "GET", "PUT", "DELETE"],
