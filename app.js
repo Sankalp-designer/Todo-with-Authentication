2844d42891917d77d9b1d9a20bd18ff541f2c388
Sankalp-designer <duttsankalp6@gmail.com>
2023-10-19 13:52:04 +0530

    removed unwanted header

--- app.js
+++ app.js
@@ -17,17 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(userRouter);
 app.use(taskRouter);
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-         res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    res.header("Access-Control-Allow-credentials", true);
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-    next();
-  });
+
 app.use(cors({
     origin: [process.env.FRONTEND_URL],
     methods: ["POST", "GET", "PUT", "DELETE"],
